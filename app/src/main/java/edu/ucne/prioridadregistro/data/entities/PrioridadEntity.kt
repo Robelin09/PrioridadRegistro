14492f31aa6b930443a95bc6460bca33e53fe5b4
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-09-01 22:25:17 -0400

    Creando PrioridadEntity

---
+++ app/src/main/java/edu/ucne/prioridadregistro/data/entities/PrioridadEntity.kt
@@ -0,0 +1,13 @@
+package edu.ucne.prioridadregistro.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="Prioridades")
+
+data class PrioridadEntity(
+    @PrimaryKey
+    val prioridadid: Int? = null,
+    val descripcion: String = "",
+    val diascompromiso: String = ""
+)
