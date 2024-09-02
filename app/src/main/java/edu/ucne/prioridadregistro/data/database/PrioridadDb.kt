b84bafcf3d146b141beb0b3868e5d58d6cfdb6ca
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-09-01 22:25:49 -0400

    Creando PrioridadDbDao

---
+++ app/src/main/java/edu/ucne/prioridadregistro/data/database/PrioridadDb.kt
@@ -0,0 +1,17 @@
+package edu.ucne.prioridadregistro.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import edu.ucne.prioridadregistro.data.dao.PrioridadDao
+import edu.ucne.prioridadregistro.data.entities.PrioridadEntity
+
+@Database(
+    entities = [
+        PrioridadEntity::class
+    ],
+    version=1,
+    exportSchema=false
+)
+abstract class PrioridadDb : RoomDatabase(){
+    abstract fun PrioridadDao(): PrioridadDao
+}
