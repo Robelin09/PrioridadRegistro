3b9ebf4d27aa93401adf45deff38936d867ba242
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-09-17 16:35:56 -0400

    Edité el MainActivity

--- app/src/main/java/edu/ucne/prioridadregistro/MainActivity.kt
+++ app/src/main/java/edu/ucne/prioridadregistro/MainActivity.kt
@@ -4,30 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.outlined.Build
+import androidx.compose.material.icons.outlined.Info
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
-import edu.ucne.prioridadregistro.data.database.PrioridadDb
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.prioridadregistro.data.local.database.PrioridadDb
 import edu.ucne.prioridadregistro.presentation.navigation.PrioridadNavHost
 import edu.ucne.prioridadregistro.ui.theme.PrioridadRegistroTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private lateinit var prioridadDb: PrioridadDb
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        prioridadDb = Room.databaseBuilder(
-            applicationContext,
-            PrioridadDb::class.java,
-            "Prioridad.db"
-        ).fallbackToDestructiveMigration().build()
         setContent {
             PrioridadRegistroTheme {
                 val navHost = rememberNavController()
-                PrioridadNavHost(prioridadDb, navHost)
+                val items = NavigationItems()
+                PrioridadNavHost(navHost, items)
+
             }
         }
     }
-
+}
+fun NavigationItems():List<NavigationItem>{
+    return listOf(
+        NavigationItem(
+            titulo = "Prioridades",
+            SelectIcon = Icons.Filled.Info,
+            UnSelectIcon = Icons.Outlined.Info
+        ),
+        NavigationItem(
+            titulo = "Tickets",
+            SelectIcon = Icons.Filled.Build,
+            UnSelectIcon = Icons.Outlined.Build
+        ),
+    )
 }
