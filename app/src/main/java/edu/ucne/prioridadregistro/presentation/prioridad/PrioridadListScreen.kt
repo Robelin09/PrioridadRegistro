685f41d7e0a1424c3ba220469a70992d16365fab
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-09-17 16:37:11 -0400

    Edité el PriodidadListScreen

--- app/src/main/java/edu/ucne/prioridadregistro/presentation/prioridad/PrioridadListScreen.kt
+++ app/src/main/java/edu/ucne/prioridadregistro/presentation/prioridad/PrioridadListScreen.kt
@@ -11,27 +11,72 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import edu.ucne.prioridadregistro.data.entities.PrioridadEntity
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.prioridadregistro.data.local.entities.PrioridadEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun PrioridadListScreen(
-    prioridadList: List<PrioridadEntity>,
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    viewModel: PrioridadViewModel = hiltViewModel(),
+    onPrioridadClick: (Int) -> Unit,
     createPrioridad: () -> Unit,
-    deletePrioridad: (PrioridadEntity) -> Unit,
-    onPrioridadClick: (Int) -> Unit
+
+) {
+val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    PrioridadListBodyScreen(
+        drawerState = drawerState,
+        scope = scope,
+        uiState,
+        onPrioridadClick,
+        createPrioridad,
+    )
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PrioridadListBodyScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    uiState: Uistate,
+    onPrioridadClick: (Int) -> Unit,
+    createPrioridad: () -> Unit,
 ) {
     Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Lista de Prioridades")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        scope.launch {
+                            drawerState.open()
+                        }
+                    }) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Ir al Menu")
+                    }
+                })
+        },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = createPrioridad,
@@ -75,30 +120,31 @@ fun PrioridadListScreen(
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
             ) {
-                items(prioridadList) { prioridad ->
-                    PrioridadRow(prioridad, deletePrioridad, onPrioridadClick)
+                items(uiState.prioridades) { prioridad ->
+                    PrioridadRow(prioridad, onPrioridadClick)
                 }
             }
         }
     }
 }
+
+
 @Composable
 fun PrioridadRow(
     prioridad: PrioridadEntity,
-    deletePrioridad: (PrioridadEntity) -> Unit,
     onPrioridadClick: (Int) -> Unit
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.clickable { onPrioridadClick(prioridad.prioridadid!!) }
+        modifier = Modifier.clickable { onPrioridadClick(prioridad.prioridadId!!) }
     ) {
-        Text(modifier = Modifier.weight(2f), text = prioridad.prioridadid.toString())
+        Text(modifier = Modifier.weight(2f), text = prioridad.prioridadId.toString())
         Text(
             modifier = Modifier.weight(2f),
             text = prioridad.descripcion,
             style = MaterialTheme.typography.headlineSmall
         )
-        Text(modifier = Modifier.weight(2f), text = prioridad.diascompromiso.toString())
+        Text(modifier = Modifier.weight(2f), text = prioridad.diasCompromiso.toString())
 
     }
     HorizontalDivider()
