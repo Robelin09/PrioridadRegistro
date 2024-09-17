d3300dc8da91e822b8b749e26ef916461c42335f
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-09-17 16:37:23 -0400

    Edité el PriodidadNavHost

--- app/src/main/java/edu/ucne/prioridadregistro/presentation/navigation/PrioridadNavHost.kt
+++ app/src/main/java/edu/ucne/prioridadregistro/presentation/navigation/PrioridadNavHost.kt
@@ -1,9 +1,20 @@
 package edu.ucne.prioridadregistro.presentation.navigation
 
 
+import android.widget.RemoteViews.RemoteCollectionItems
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -11,62 +22,107 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
-import edu.ucne.prioridadregistro.data.database.PrioridadDb
+import edu.ucne.prioridadregistro.NavigationItem
+import edu.ucne.prioridadregistro.data.local.database.PrioridadDb
 import edu.ucne.prioridadregistro.presentation.prioridad.PrioridadDetailsScreen
 import edu.ucne.prioridadregistro.presentation.prioridad.PrioridadListScreen
 import edu.ucne.prioridadregistro.presentation.prioridad.PrioridadScreen
+import edu.ucne.prioridadregistro.presentation.ticket.TicketDetailsScreen
+import edu.ucne.prioridadregistro.presentation.ticket.TicketListScreen
+import edu.ucne.prioridadregistro.presentation.ticket.TicketScreen
 import kotlinx.coroutines.launch
 
 @Composable
 fun PrioridadNavHost(
-    prioridadDb: PrioridadDb,
     navHostController: NavHostController,
+    items: List<NavigationItem>
 ) {
-    val lifecycleOwner = LocalLifecycleOwner.current
-    val prioridadList by prioridadDb.prioridadDao().getAll()
-        .collectAsStateWithLifecycle(
-            initialValue = emptyList(),
-            lifecycleOwner = lifecycleOwner,
-            minActiveState = Lifecycle.State.STARTED
-        )
     val scope = rememberCoroutineScope()
-
-    NavHost(
-        navController = navHostController,
-        startDestination = Screen.PrioridadList
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    var SelectedItem by rememberSaveable {
+        mutableStateOf(0)
+    }
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {
+                Text(text = "Menu")
+                items.forEachIndexed { index, navigationItem ->
+                    NavigationDrawerItem(
+                        label = { Text(text = navigationItem.titulo) },
+                        selected = index == SelectedItem,
+                        onClick = {
+                            scope.launch { drawerState.close() }
+                            when(navigationItem.titulo){
+                                "Prioridades" -> {navHostController.navigate(Screen.PrioridadList)}
+                                "Tickets" -> {navHostController.navigate(Screen.TicketList)}
+                            }
+                        }
+                    )
+                }
+            }
+        },
+        drawerState = drawerState
     ) {
-        composable<Screen.PrioridadList> {
-            PrioridadListScreen(
-                prioridadList,
-                createPrioridad = {
-                    navHostController.navigate(Screen.Prioridad(0))
-                },
-                deletePrioridad = { prioridad ->
-                    scope.launch {
-                        prioridadDb.prioridadDao().delete(prioridad)
+        NavHost(
+            navController = navHostController,
+            startDestination = Screen.TicketList
+        ) {
+            composable<Screen.PrioridadList> {
+                PrioridadListScreen(
+                    drawerState = drawerState,
+                    scope = scope,
+                    createPrioridad = {
+                        navHostController.navigate(Screen.Prioridad(0))
+                    },
+                    onPrioridadClick = {
+                        navHostController.navigate(Screen.PrioridadDetails(it))
                     }
-                },
-                onPrioridadClick = { prioridadId ->
-                    navHostController.navigate(Screen.PrioridadDetails(prioridadId))
-                }
-            )
-        }
-        composable<Screen.Prioridad> {
-            val args = it.toRoute<Screen.Prioridad>()
-            PrioridadScreen(
-                prioridadDb,
-                goBack = {
-                    navHostController.navigateUp()
-                }
-            )
-        }
-        composable<Screen.PrioridadDetails> { backStackEntry ->
-            val args = backStackEntry.toRoute<Screen.PrioridadDetails>()
-            PrioridadDetailsScreen(
-                prioridadDb,
-                prioridadId = args.prioridadId,
-                goBack = { navHostController.navigateUp() }
-            )
+                )
+            }
+            composable<Screen.TicketList> {
+                TicketListScreen(
+                    drawerState = drawerState,
+                    scope = scope,
+                    createTicket = {
+                        navHostController.navigate(Screen.Ticket(0))
+                    },
+                    onTicketClick = {
+                        navHostController.navigate(Screen.TicketDetails(it))
+                    },
+
+                    )
+            }
+
+            composable<Screen.Prioridad> {
+                val args = it.toRoute<Screen.Prioridad>()
+                PrioridadScreen(
+                    goBack = {
+                        navHostController.navigateUp()
+                    }
+                )
+            }
+            composable<Screen.PrioridadDetails> { backStackEntry ->
+                val args = backStackEntry.toRoute<Screen.PrioridadDetails>()
+                PrioridadDetailsScreen(
+                    prioridadId = args.prioridadId,
+                    goBack = { navHostController.navigateUp() }
+                )
+            }
+            composable<Screen.Ticket> {
+                val args = it.toRoute<Screen.Ticket>()
+                TicketScreen(
+                    goBack = {
+                        navHostController.navigateUp()
+                    }
+                )
+            }
+            composable<Screen.TicketDetails> {
+                val args = it.toRoute<Screen.TicketDetails>()
+                TicketDetailsScreen(
+                    ticketId = args.ticketId,
+                    goBack = { navHostController.navigateUp() }
+                )
+            }
         }
     }
 }
