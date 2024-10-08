fd8c96e5690405e82e199f77613709988d8d9e03
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-10-08 13:05:05 -0400

    Editando el TicketScreen.kt

--- app/src/main/java/edu/ucne/prioridadregistro/presentation/ticket/TicketScreen.kt
+++ app/src/main/java/edu/ucne/prioridadregistro/presentation/ticket/TicketScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import java.text.SimpleDateFormat
+import java.util.Date
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -37,6 +39,8 @@ fun TicketScreen(
     TicketBodyScreen(
         uiState = uiState,
         onClienteChange = viewModel::onClienteChange,
+        onSistemaChange = viewModel::onSistemaChange,
+        onSolicitadoPorChange = viewModel::onSolicitadoPorChange,
         onAsuntoChange = viewModel::onAsuntoChange,
         onDescripcionChange = viewModel::onDescripcionChange,
         onSaveTicket = viewModel::save,
@@ -50,7 +54,9 @@ fun TicketScreen(
 @Composable
 fun TicketBodyScreen(
     uiState: TicketUiState,
-    onClienteChange: (String) -> Unit,
+    onClienteChange: (Int) -> Unit,
+    onSistemaChange: (Int) -> Unit,
+    onSolicitadoPorChange: (String) -> Unit,
     onAsuntoChange: (String) -> Unit,
     onDescripcionChange: (String) -> Unit,
     onSaveTicket: () -> Unit,
@@ -58,6 +64,9 @@ fun TicketBodyScreen(
     onPrioridadChange: (Int) -> Unit,
     goBack: () -> Unit
 ) {
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy")
+    val formattedDate = dateFormat.format(uiState.fecha ?: Date()) // Usa la fecha actual si uiState.fecha es null
+
     Scaffold { innerPadding ->
         Column(
             modifier = Modifier
@@ -65,12 +74,24 @@ fun TicketBodyScreen(
                 .padding(8.dp)
                 .fillMaxSize()
         ) {
+            ClienteDropdown(uiState = uiState, onClienteChange = onClienteChange)
+            SistemaDropdown(uiState = uiState, onSistemaChange = onSistemaChange)
+
             OutlinedTextField(
-                label = { Text("Cliente") },
-                value = uiState.cliente,
-                onValueChange = onClienteChange,
+                label = { Text("Solicitado por") },
+                value = uiState.solicitadoPor,
+                onValueChange = onSolicitadoPorChange,
                 modifier = Modifier.fillMaxWidth()
             )
+
+            OutlinedTextField(
+                label = { Text("Fecha") },
+                value = formattedDate,
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                readOnly = true
+            )
+
             OutlinedTextField(
                 label = { Text("Asunto") },
                 value = uiState.asunto,
@@ -83,40 +104,8 @@ fun TicketBodyScreen(
                 onValueChange = onDescripcionChange,
                 modifier = Modifier.fillMaxWidth()
             )
-            var expanded by remember { mutableStateOf(false) }
-            var selectedText by remember { mutableStateOf("Selecciona Prioridad") }
 
-            ExposedDropdownMenuBox(
-                expanded = expanded,
-                onExpandedChange = {
-                    expanded = !expanded
-                }
-            ) {
-                OutlinedTextField(
-                    value = selectedText,
-                    onValueChange = {},
-                    modifier = Modifier
-                        .menuAnchor()
-                        .fillMaxWidth(),
-                    readOnly = true,
-                    label = { Text("Prioridad") }
-                )
-                ExposedDropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false }
-                ) {
-                    uiState.prioridades.forEach { prioridad ->
-                        DropdownMenuItem(
-                            text = { Text(prioridad.descripcion) },
-                            onClick = {
-                                selectedText = prioridad.descripcion
-                                prioridad.prioridadId?.let { onPrioridadChange(it) }
-                                expanded = false
-                            }
-                        )
-                    }
-                }
-            }
+            PrioridadDropdown(uiState = uiState, onPrioridadChange = onPrioridadChange)
 
             Spacer(modifier = Modifier.padding(2.dp))
             uiState.errorMessage?.let {
@@ -147,5 +136,132 @@ fun TicketBodyScreen(
             }
             Spacer(modifier = Modifier.padding(8.dp))
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ClienteDropdown(
+    uiState: TicketUiState,
+    onClienteChange: (Int) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf("Selecciona Cliente") }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = {},
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth(),
+            readOnly = true,
+            label = { Text("Cliente") }
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            uiState.clientes.forEach { cliente ->
+                DropdownMenuItem(
+                    text = { Text(cliente.nombres) },
+                    onClick = {
+                        selectedText = cliente.nombres
+                        cliente.clienteId?.let { onClienteChange(it) }
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SistemaDropdown(
+    uiState: TicketUiState,
+    onSistemaChange: (Int) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf("Selecciona Sistema") }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = {},
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth(),
+            readOnly = true,
+            label = { Text("Sistema") }
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            uiState.sistemas.forEach { sistema ->
+                DropdownMenuItem(
+                    text = { Text(sistema.nombredeSistema) },
+                    onClick = {
+                        selectedText = sistema.nombredeSistema
+                        sistema.sistemaId?.let { onSistemaChange(it) }
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PrioridadDropdown(
+    uiState: TicketUiState,
+    onPrioridadChange: (Int) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf("Selecciona Prioridad") }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        OutlinedTextField(
+            value = selectedText,
+            onValueChange = {},
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth(),
+            readOnly = true,
+            label = { Text("Prioridad") }
+        )
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            uiState.prioridades.forEach { prioridad ->
+                DropdownMenuItem(
+                    text = { Text(prioridad.descripcion) },
+                    onClick = {
+                        selectedText = prioridad.descripcion
+                        prioridad.prioridadId?.let { onPrioridadChange(it) }
+                        expanded = false
+                    }
+                )
+            }
+        }
+
     }
 }
