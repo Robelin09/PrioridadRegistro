88329ca3ae6fa01daa56919c572230599cf86ddb
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-10-08 12:57:14 -0400

    Agregando ClienteDto.kt

---
+++ app/src/main/java/edu/ucne/prioridadregistro/data/remote/dto/ClienteDto.kt
@@ -0,0 +1,11 @@
+package edu.ucne.prioridadregistro.data.remote.dto
+
+data class ClienteDto(
+    val clienteId: Int?,
+    val nombres: String,
+    val telefono: String,
+    val celular : String,
+    val rnc: String,
+    val email: String,
+    val direccion: String
+)
