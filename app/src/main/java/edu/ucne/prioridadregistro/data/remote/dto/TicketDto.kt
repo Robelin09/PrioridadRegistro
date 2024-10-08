dde7f889f20e0ecbcc03d79776b2bd5c946a7568
Robelin Isaac Concepción Henriquez <robelin_concepcion@ucne.edu.do>
2024-10-08 12:56:16 -0400

    Agregando TicketDto.kt

---
+++ app/src/main/java/edu/ucne/prioridadregistro/data/remote/dto/TicketDto.kt
@@ -0,0 +1,14 @@
+package edu.ucne.prioridadregistro.data.remote.dto
+
+import java.util.Date
+
+data class TicketDto(
+    val ticketId: Int?,
+    val fecha: Date,
+    val clienteId: Int,
+    val sistemaId: Int,
+    val prioridadId: Int,
+    val solicitadoPor: String,
+    val asunto: String,
+    val descripcion: String
+)
